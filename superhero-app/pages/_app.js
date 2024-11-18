1c499a444b870d577e98e771db2d207bc4d52a0f
Pedro Henrique <155556494+Pehzinz@users.noreply.github.com>
2024-11-18 17:07:32 -0300

    Update _app.js

--- superhero-app/pages/_app.js
+++ superhero-app/pages/_app.js
@@ -1,4 +1,6 @@
 //Este arquivo é responsável por inicializar a aplicação Next.js e importar os estilos globais.
+//Pedro Henrique Gonçalves - RA: 10439486
+
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
