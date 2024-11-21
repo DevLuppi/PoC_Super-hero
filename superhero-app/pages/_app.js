98b93a985c51bee3b1c61cfb22c6b6f363efa3d3
Leonardo Luppi <88064328+DevLuppi@users.noreply.github.com>
2024-11-21 20:43:52 -0300

    _app.js

--- superhero-app/pages/_app.js
+++ superhero-app/pages/_app.js
@@ -1,5 +1,6 @@
 //Este arquivo é responsável por inicializar a aplicação Next.js e importar os estilos globais.
 //Pedro Henrique Gonçalves - RA: 10439486
+//Leonardo Luppi Vanni - RA: 10436603
 
 import '../styles/globals.css';
 
